a65ba6b71c919aa21d87d42539f65f8ba1a561f4
Piotr Teterwak <piotr@graphlab.com>
2015-06-02 16:20:22 -0700

    Typos
    Change-Id: I3644e5a68839a2338fc36f37603e1e914d45129c

--- src/nnet/nnet_impl.cpp
+++ src/nnet/nnet_impl.cpp
@@ -8,12 +8,12 @@ namespace nnet {
 template<>
 GLINetTrainer* CreateNet<cpu>(int net_type) {
   return CreateNet_<cpu>(net_type);
+}
 #if MSHADOW_USE_CUDA == 0 
 GLINetTrainer* CreateNet<gpu>(int net_type) {
   utils::Error("CUDA is not supported");
   return NULL;
 }
 #endif
-}
 }  // namespace nnet
 }  // namespace cxxnet
