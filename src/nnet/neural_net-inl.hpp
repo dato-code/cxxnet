e5fabddfd248cc5f28a1f1befd793230ee9ebc7b
Piotr Teterwak <piotr@graphlab.com>
2015-06-03 19:19:21 -0700

    -Added check in case of empty config string
    Change-Id: I920c2ccf1844760f0eed881328cf3f85c3d19c34

--- src/nnet/neural_net-inl.hpp
+++ src/nnet/neural_net-inl.hpp
@@ -239,7 +239,9 @@ struct NeuralNet {
     nodes.resize(cfg.param.num_nodes);
     mshadow::Shape<3> s = cfg.param.input_shape;
     // setup input shape
-    nodes[0].data.shape_ = mshadow::Shape4(max_batch, s[0], s[1], s[2]);
+    if nodes(nodes.size() > 0){
+      nodes[0].data.shape_ = mshadow::Shape4(max_batch, s[0], s[1], s[2]);
+    }
     // setup extra data
     for (int i = 0; i < cfg.param.extra_data_num; ++i) {
       const std::vector<int>& extra_shape = cfg.extra_shape;
