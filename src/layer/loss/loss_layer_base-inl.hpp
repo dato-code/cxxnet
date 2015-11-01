95811c6969eb8eee8f86fb0406dfbd30c90e5c12
Piotr Teterwak <piotr@graphlab.com>
2015-11-01 13:57:52 -0800

    pointers -> const pointers in reinterpret cast

--- src/layer/loss/loss_layer_base-inl.hpp
+++ src/layer/loss/loss_layer_base-inl.hpp
@@ -29,8 +29,8 @@ class LossLayerBase: public ILayer<xpu> {
       //using param value as serialization for class weights
       // First double in contiguous array of double is size of the rest 
       // of the array.
-      size_t class_weights_size =static_cast<size_t>(*(reinterpret_cast<double*>(val)));
-      double* class_weights_pointer = reinterpret_cast<double*>(val) + 1;
+      size_t class_weights_size =static_cast<size_t>(*(reinterpret_cast<const double*>(val)));
+      double* class_weights_pointer = reinterpret_cast<const double*>(val) + 1;
       class_weights_vector.assign(class_weights_pointer, class_weights_pointer + class_weights_size);
       //class_weights_vector.assign(num_classes + 1, num_classes + (size_t)*num_classes + 1);
     } 
